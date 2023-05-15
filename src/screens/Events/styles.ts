61302387f20a1d3728a6b901146c485b34a580f1
Ivan Luis Seibel <ivanseibel@gmail.com>
2023-05-16 00:00:00 +0100

    Rename Events to Groups

--- src/screens/Events/styles.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components/native';
-import { ThemeType } from 'styled-components';
-
-export const Container = styled.View`
-  flex: 1;
-  background-color: ${({ theme }: { theme: ThemeType }) => theme.COLORS.GRAY_600};
-  padding: 24px;
-`;
