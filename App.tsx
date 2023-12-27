5c31b53e8a4ee418a05fbf44868e92d4f245ddfe
Ivan Luis Seibel <ivanseibel@gmail.com>
2023-12-27 22:04:31 +0000

    Create Loading component

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { Events } from '@screens/Events'
 import { ThemeProvider } from 'styled-components'
 import defaultTheme from './src/theme'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
-import { ActivityIndicator } from 'react-native'
+import { Loading } from '@components/Loading'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -12,7 +12,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={ defaultTheme }>
-      { !fontsLoaded ? <ActivityIndicator /> : <Events /> }
+      { !fontsLoaded ? <Loading /> : <Events /> }
     </ThemeProvider>
   );
 }
