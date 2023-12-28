b242b0c9dd7fc5a9daa2dd3dce4ada524c19fefa
Ivan Luis Seibel <ivanseibel@gmail.com>
2023-12-28 13:58:03 +0000

    Rename Events to Groups

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,15 @@
+import { Header } from "@components/Header";
+import { Container } from "./styles";
+import { Highlight } from "@components/Highlight";
+
+export function Groups() {
+  return (
+    <Container>
+      <Header />
+      <Highlight 
+        title="Groups"
+        subtitle="Play with your friends"
+      />
+    </Container>
+  );
+}
