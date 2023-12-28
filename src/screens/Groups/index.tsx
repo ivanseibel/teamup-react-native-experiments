fdc55548d0ac644695530bddbbc5b6c6754c5952
Ivan Luis Seibel <ivanseibel@gmail.com>
2023-12-28 14:36:22 +0000

    Render a list of groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,8 +2,48 @@ import { Header } from "@components/Header";
 import { Container } from "./styles";
 import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
+import { useState } from "react";
+import { FlatList } from "react-native";
+
+type Group = {
+  id: string;
+  title: string;
+}
+
+const INITIAL_GROUPS = [
+  {
+    id: '1',
+    title: 'Sunday Football'
+  },
+  {
+    id: '2',
+    title: 'Saturday Football'
+  },
+  {
+    id: '3',
+    title: 'Friday Football'
+  },
+  {
+    id: '4',
+    title: 'Thursday Football'
+  },
+  {
+    id: '5',
+    title: 'Wednesday Football'
+  },
+  {
+    id: '6',
+    title: 'Tuesday Football'
+  },
+  {
+    id: '7',
+    title: 'Monday Football'
+  },
+];
 
 export function Groups() {
+  const [groups, setGroups] = useState<Group[]>(INITIAL_GROUPS);
+
   return (
     <Container>
       <Header />
@@ -11,8 +51,14 @@ export function Groups() {
         title="Groups"
         subtitle="Play with your friends"
       />
-      <GroupCard 
-        title="Sunday Football"
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <GroupCard 
+            title={item.title}
+          />
+        )}
       />
     </Container>
   );
