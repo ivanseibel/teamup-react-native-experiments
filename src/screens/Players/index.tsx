ebf9f26a2c93706a671f0d7ff263aa14b061888b
Ivan Luis Seibel <ivanseibel@gmail.com>
2023-12-28 20:08:43 +0000

    Add PlayerCard to Players screen

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { FlatList } from "react-native";
+import { FlatList, View } from "react-native";
 
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
@@ -8,6 +8,7 @@ import { Input } from "@components/Input";
 import { Filter } from "@components/Filter";
 
 import { Container, Form, HeaderList, PlayersNumber } from "./styles";
+import { PlayerCard } from "@components/PlayerCard";
 
 type Teams = 'Team A' | 'Team B';
 
@@ -17,9 +18,73 @@ type Player = {
   team: string;
 }
 
+const PLAYERS: Player[] = [
+  {
+    id: '1',
+    name: 'Player 1',
+    team: 'Team A'
+  },
+  {
+    id: '2',
+    name: 'Player 2',
+    team: 'Team B'
+  },
+  {
+    id: '3',
+    name: 'Player 3',
+    team: 'Team A'
+  },
+  {
+    id: '4',
+    name: 'Player 4',
+    team: 'Team B'
+  },
+ 
+  {
+    id: '9',
+    name: 'Player 9',
+    team: 'Team A'
+  },
+  {
+    id: '10',
+    name: 'Player 10',
+    team: 'Team B'
+  },
+  {
+    id: '11',
+    name: 'Player 11',
+    team: 'Team A'
+  },
+  {
+    id: '12',
+    name: 'Player 12',
+    team: 'Team B'
+  },
+  {
+    id: '15',
+    name: 'Player 15',
+    team: 'Team A'
+  },
+  {
+    id: '16',
+    name: 'Player 16',
+    team: 'Team B'
+  },
+  {
+    id: '17',
+    name: 'Player 17',
+    team: 'Team A'
+  },
+  {
+    id: '18',
+    name: 'Player 18',
+    team: 'Team B'
+  },
+];
+
 export function Players() {
   const [team, setTeam] = useState<Teams>('Team A');
-  const [players, setPlayers] = useState<string[]>([]);
+  const [players, setPlayers] = useState<Player[]>(PLAYERS);
 
   return (
     <Container>
@@ -60,6 +125,17 @@ export function Players() {
           {players.length}
         </PlayersNumber>
       </HeaderList>
+
+      <FlatList 
+        data={players.filter((player) => player.team === team)}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <PlayerCard 
+            name={item.name}
+            onRemove={() => {}}
+          />
+        )}
+      />
 
     </Container>
   )
