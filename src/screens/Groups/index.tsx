13d08bd651495aba2915b7534ec1a6a3cdbea773
Ivan Seibel <me@ivanseibel.dev>
2023-12-29 15:55:30 +0000

    Fix import for EmptyList

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,10 +2,8 @@ import { Header } from "@components/Header";
 import { Container } from "./styles";
 import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
-import { useState } from "react";
-import { Alert, FlatList } from "react-native";
-import { EmptyList } from '../../components/EmptyList';
 import { Button } from "@components/Button";
+import { EmptyList } from "@components/EmptyList";
 
 type Group = {
   id: string;
