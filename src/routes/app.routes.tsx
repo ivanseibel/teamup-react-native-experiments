19815271b83d21c0ef4a300de342ddf58943deaf
Ivan Seibel <me@ivanseibel.dev>
2023-12-29 15:53:02 +0000

    Create routes

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,21 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import App from '../../App';
+import { Groups } from "@screens/Groups";
+import { NewGroup } from "@screens/NewGroup";
+import { Players } from "@screens/Players";
+
+const { Navigator, Screen } = createNativeStackNavigator();
+
+export function AppRoutes() {
+  return (
+    <Navigator
+      screenOptions={{
+        headerShown: false
+      }}
+    >
+      <Screen name="Groups" component={Groups} />
+      <Screen name="NewGroup" component={NewGroup} />
+      <Screen name="Players" component={Players} />
+    </Navigator>
+  );
+}
