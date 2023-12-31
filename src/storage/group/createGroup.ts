054834ed16c1da609c79d9b76b708d4a17cb0251
Ivan Luis Seibel <ivanseibel@gmail.com>
2023-12-31 20:14:42 +0000

    Remove unnecessary import

--- src/storage/group/createGroup.ts
+++ src/storage/group/createGroup.ts
@@ -1,5 +1,4 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { Alert } from 'react-native';
 
 import { GROUP_COLLECTION } from '@storage/storageConfig'; 
 import { getAllGroups } from './getAllGroups';
