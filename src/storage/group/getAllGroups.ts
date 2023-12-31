76b3dfc14af0d122bc01a44addd22cafbdec43fd
Ivan Luis Seibel <ivanseibel@gmail.com>
2023-12-31 20:15:05 +0000

    Refactor to improve readability

--- src/storage/group/getAllGroups.ts
+++ src/storage/group/getAllGroups.ts
@@ -1,16 +1,14 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { GROUP_COLLECTION } from "@storage/storageConfig";
-import { Group } from "./createGroup";
+import { Group } from "./GroupStorageDTO";
 
 export async function getAllGroups() {
   try {
-    const groups = await AsyncStorage.getItem(GROUP_COLLECTION);
+    const storage = await AsyncStorage.getItem(GROUP_COLLECTION);
   
-    if (groups) {
-      return JSON.parse(groups) as Group[];
-    }
-  
-    return [];
+    const groups: Group[] = storage ? JSON.parse(storage) : [];
+
+    return groups;
   } catch (err) {
     throw err;
   }
